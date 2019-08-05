c10995b964368c5a59690da4700f793195556b6e
imsat <worksatyajit@gmail.com>
2019-08-06 03:08:53 +0600

    Full completed Multiple vue image upload

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-9">
                 <div class="card">
-                    <div class="card-header">Example Component</div>
+                    <div class="card-header">Multiple Image Uploader</div>
 
                     <div class="card-body">
-                        I'm an example component.
+                        <image-uploader></image-uploader>
                     </div>
                 </div>
             </div>
@@ -15,9 +15,10 @@
 </template>
 
 <script>
+    import ImageUploader from './ImageUploader';
     export default {
-        mounted() {
-            console.log('Component mounted.')
+        components: {
+            ImageUploader
         }
     }
 </script>
